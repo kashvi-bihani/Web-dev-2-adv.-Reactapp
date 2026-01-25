42632fa683d97f1403e77ccaab57a0e3e1527a32
Kashvi Bihani <kashvi@example.com>
2026-01-25 19:35:25 +0530

    Initial commit - HTML and JavaScript files

---
+++ practice.js
@@ -0,0 +1,17 @@
+let heading= document.getElementById("head");
+console.log(heading.textContent)
+let paragraph =document.getElementsByClassName("Para");
+console.log(paragraph) 
+const para=document.querySelector(".color");
+para.style.color="Blue";
+let all=document.querySelectorAll(".all");
+all.forEach(element => {console.log(all)  
+});
+
+
+
+
+
+
+
+

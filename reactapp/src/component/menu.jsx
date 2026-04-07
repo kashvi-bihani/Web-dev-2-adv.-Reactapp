d9088ccec1eec426832f11dd7788e86886f94e89
Kashvi Bihani <kashvi@example.com>
2026-04-07 10:17:58 +0530

    Updated files

--- reactapp/src/component/menu.jsx
+++ reactapp/src/component/menu.jsx
@@ -1,23 +1,50 @@
-import React from "react"
+//import React from "react"
 
-function Menu(props){
-    return(
-    <>
-    <div>Menu</div>
-    <ul>
-        <li>Entertainment</li>
-        <li>Politics</li>
-        <li>Tech</li>
+//function Menu(props){
+    //return(
+    //<>
+    //<div>Menu</div>
+    //<ul>
+        //<li>Entertainment</li>
+        //<li>Politics</li>
+        //<li>Tech</li>
+        //</ul>
+        //<button
+         // className="counter"
+          //onClick={() => setCount((count) => count + 1)}
+        //>
+          //Count is {props.count}
+        //</button>
+   // </>
+    //)
+//}
+ //export default Menu 
+
+ import React, { Component } from "react";
+
+export default class Menu extends Component {
+  constructor(props) {
+    super(props);   
+  }
+
+  render() {        
+    return (
+      <>
+        <div>Menu</div>
+        <ul>
+          <li>Entertainment</li>
+          <li>Politics</li>
+          <li>Tech</li>
         </ul>
+
         <button
           className="counter"
-          onClick={() => setCount((count) => count + 1)}
+          onClick={() => this.props.setCount((count) => count + 1)} 
         >
-          Count is {props.count}
+          Count is {this.props.count} 
         </button>
-    </>
-    )
+      </>
+    );
+  }
 }
-export default Menu 
 
-import React,{Component} from "react"

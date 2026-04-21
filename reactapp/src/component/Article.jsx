6798372389cebf6dbeafcbdb5ebc64f18db16274
Kashvi Bihani <2501730117@krmu.edu.in>
2026-04-21 21:59:16 +0530

    Fetch Data Code

--- reactapp/src/component/Article.jsx
+++ reactapp/src/component/Article.jsx
@@ -1,14 +1,50 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import ArtStyle from "../css/Article.module.css"
 // import styled from "styled-components"
 import styled from "@emotion/styled"
 import { css } from '@emotion/react'
+import axios from "axios"
 
 
 function Article(props) {
     const[count,setCount]=useState(0)
     const[age,setAge]=useState(0)
-    const[name,setName]=useState("")
+    const[name,setName]=useState("Alex")
+
+    useEffect(()=>{
+      console.log("Component Mounted")
+      return ()=>{
+        console.log("Component Unmounted")
+      }
+    },[ name])
+
+    useState(()=>{
+      // const fetchData=async()=>{
+      //   try{
+      //     const response=await fetch("https://dummyjson.com/products/222",{
+      //       method:"GET"
+      //     })
+      //     const data=await response.json()
+      //     console.log(data)
+      //   }catch(error){
+      //     console.log(error)
+      //   }
+      // }
+
+      const fetchData=async()=>{
+        try{
+          const response=await axios.get("https://dummyjson.com/products")
+          console.log(response.data)
+        }catch(error){
+          console.log(error)
+        }
+      }
+
+      fetchData()
+    },[])
+
+    
+
 
     const handleIncrement=()=>{
         setCount(count+1)
@@ -18,13 +54,15 @@ function Article(props) {
     <div>
       <h2>Article</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque maiores nemo obcaecati a quam nihil nobis enim quod cupiditate, ipsam sunt laborum vel numquam, sint accusantium. Vitae aliquam esse culpa.</p>
-      <h3>Count</h3>
       <h3 style={{backgroundColor:"red"}}>{props.data}</h3>
       <h2>{count}</h2>
       <button className={ArtStyle.btn} onClick={handleIncrement}>Increment</button>
+      <h2>{name}</h2>
+      <button className='bg-yellow-900' onClick={()=>setName("John")}>Change Name</button>
       <Button>Click Me!</Button>
       <Button red >Click Again</Button>
       <button css={{backgroundColor:"green",color:"white",borderRadius:"10px",height:"30px",width:"150px",'&:hover':{backgroundColor:"blue"}}}>Css Prop Button</button>
+
       <button className='bg-pink-500 text-white border-2 border-white'>Tailwind Button</button>
     </div>
   )
@@ -40,14 +78,6 @@ const Button=styled.button`
     height: 30px;  
     width: 100px;
 `
-//useState({}=>{
- // const fetchData=>()=>{
-   // console.log("Data Fetched")
-  //}
-//fetchData()
-//})
-
-
 
 
 
